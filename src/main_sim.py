55aa4c1842d473b8e18e5b4a3e0d65546f5ab542
athennad <athennad@stud.hs-offenburg.de>
2025-07-29 12:30:27 +0200

    cleanup_mainsim
    cleanup_mainsim(and update)

--- src/main_sim.py
+++ src/main_sim.py
@@ -31,106 +31,10 @@ from src.models import chp_mosaik
 from src.models import gasboiler_mosaik
 from src.models import pvlib_model
 #______________________________moved outside method, to be accessible from other scripts(visu.ipynb)
- 
+#TODO use the param file in visu
 STEP_SIZE = 60*15 # step size 15 minutes
 HV = 10833.3 #Heating value of natural gas in Wh/m^3; standard cubic meter
 
-# # Heat pump
-# params_hp = {'hp_model': 'Air_60kW',
-#                 'heat_source': 'Air',
-#                 'calc_mode': 'fast'
-#                 }
-
-# # CHP
-# params_chp = {'eff_el': 0.54,
-#             'nom_P_th': 92_000,
-#             'mdot': 4.0,
-#             'startup_coeff' : [-2.63, 3.9, 0.57], #coefficients to model the startup behaviour, in the order : Intercept, x,x^2,x^3...
-#             'eta' : 0.5897, # fuel efficiency of chp, from datasheet.
-#             'hv' : HV
-#             }
-
-# #Gas boiler
-# params_boiler = {'eta' : 0.98, 'hv' : HV,
-#                     'nom_P_th' : [0, 74000, 148000, 222000, 296000, 370000], #Operating points of boiler, in W
-#                     'Set_Temp' : 75
-#                     }
-
-# # hot water tank
-# params_hwt = {
-#         'height': 2500,
-#         'volume': 5000,
-#         'T_env': 20.0,
-#         'htc_walls': 0.28,
-#         'htc_layers': 0.897,
-#         'n_layers': 3,
-#         'n_sensors': 3,
-#         'connections': {
-#             'heat_in': {'pos': 150},
-#             'heat_out': {'pos': 2350},
-#             'chp_in': {'pos': 2300},
-#             'chp_out': {'pos': 50},
-#             'hp_in': {'pos': 2200},
-#             'hp_out': {'pos': 100},
-#             'boiler_in' : {'pos' : 2400},
-#             'boiler_out' : {'pos' : 120},
-#             'heat_out2' : {'pos' : 2400},
-#             'heat_in2' : {'pos' : 200}
-#         },
-#         'heating_rods': {
-#             'hr_1' : {
-#                 'mode' : 'on',
-#                 'pos' : 2200,
-#                 'P_th_stages' : [0, 500, 1000, 2000, 10000],
-#                 'T_max' : 67, # could assign setpoint attr directly here!,
-#                 'eta' : 1 }}
-#     }
-
-# init_vals_hwt0 = {
-#         'layers': {'T': [40.0, 30.0, 20.0]},
-#         'hr_1': { 'P_el': 0}
-#     }
-
-# init_vals_hwt1 = {
-#         'layers': {'T': [40.0, 30.0, 20.0]},
-#         'hr_1': { 'P_el': 0}
-#     }
-
-# init_vals_hwt2 = {
-#         'layers': {'T': [80.0, 70.0, 60.0]},
-#         'hr_1': { 'P_el': 500}
-#     }
-
-
-# # Parameters for controller model
-# params_ctrl = {
-#     'T_hp_sp_winter': 50,
-#     'T_hp_sp_summer': 67,
-#     'T_hp_sp_surplus': 67,
-#     'T_chp_h' : 65,
-#     'T_dhw_sp': 65,
-#     'T_dhw_buffer': 5,
-#     'heat_rT' : 35,
-#     'operation_mode': 'heating',
-#     'control_strategy': '1',
-#     # 'hr_mode' : 'off',
-#     'supply_config' : '3-runner',
-#     'sh_out' : '1',         #0 for first tank, 1 for 2nd tank...
-#     'dhw_out' : '2',
-#     'boiler_mode': 'on',
-#     'boiler_delay' : 10 * 60, #the time after which the boiler is turned on to assist chp.
-#     'step_size' : STEP_SIZE,
-#     'params_hwt': params_hwt
-# }
-
-# export_json = {
-#     'hp' : params_hp,
-#     'chp' : params_chp,
-#     'boiler' : params_boiler,
-#     'ctrl' : params_ctrl,
-#     'tank' : params_hwt
-    
-# }
 ref_param_filename = 'ref_params.json'
 param_export_filename = 'used_params.json' # TODO pass this in des_sim parameter list
 def export2json(params_dict):
@@ -140,7 +44,7 @@ def export2json(params_dict):
 
 def generatePrefix(current_params, ref_param_name) :
     changes_dict = {}  #this dict will store the keys and their changes, could be exported with a unique key.
-    filename = os.path.join(OUTPUT_PATH, ref_param_name) # TODO could use a fixed reference params.
+    filename = os.path.join(OUTPUT_PATH, ref_param_name)
     prefix = ''
     if not os.path.exists(filename):
         prefix += '_'
@@ -168,8 +72,8 @@ def hash_encrypt(changes):
     hash_str = hashlib.sha256(change_str.encode()).hexdigest()[:8]
 
     # Optionally store the mapping for future lookup
-    with open(os.path.join(OUTPUT_PATH, f'{hash_str}_changes.json'), 'w') as f:
-        json.dump(changes, f, indent=4)
+    # with open(os.path.join(OUTPUT_PATH, f'{hash_str}_changes.json'), 'w') as f:
+        # json.dump(changes, f, indent=4)
     return hash_str
 
 def run_DES(params):
@@ -222,36 +126,12 @@ def run_DES(params):
 
 
     }
-
-    sim_config_dis = {
-    'CSV': {
-        'cmd': 'python C:\\ProgramData\\anaconda3\\envs\\des_sim\\Lib\\site-packages\\mosaik_csv.py %(addr)s'
-    },
-    'CSV_writer': {
-        'cmd': 'python C:\\ProgramData\\anaconda3\\envs\\des_sim\\Lib\\site-packages\\mosaik_csv_writer.py %(addr)s',
-    },
-    'HeatPumpSim': {
-        'cmd': 'python C:\\ProgramData\\anaconda3\\envs\\des_sim\\Lib\\site-packages\\mosaik_components\\heatpump\\Heat_Pump_mosaik.py %(addr)s',
-    },
-    'HotWaterTankSim': {
-        'cmd': 'python C:\\ProgramData\\anaconda3\\envs\\des_sim\\Lib\\site-packages\\mosaik_components\\heatpump\\hotwatertank\\hotwatertank_mosaik.py %(addr)s',
-    },
-    'ControllerSim': {
-        'cmd': 'python models/controller_mosaik.py %(addr)s',
-    },
-    'CHPSim': {
-        'cmd': 'python models/chp_mosaik.py %(addr)s',
-    },
-    'PVSim': {
-        'cmd': 'python C:\\ProgramData\\anaconda3\\envs\\des_sim\\Lib\\site-packages\\mosaik_components\\pv\\pvsimulator.py %(addr)s',
-    },
-    'Boilersim': {
-        'cmd': 'python models/gasboiler_mosaik.py %(addr)s',
-    },
-}
-
     
-    
+    # Create World
+    world = mosaik.World(sim_config)
+    START = '2022-01-01 00:00:00'
+    END =  365*24*60*60 # one year in seconds
+
     # unpacking input params
     params_boiler = params['boiler']
     params_hp = params['hp']
@@ -260,41 +140,21 @@ def run_DES(params):
     params_hwt = params['tank']
     init_vals_hwt0 = params['init_vals_tank']['init_vals_hwt0']
     init_vals_hwt1 = params['init_vals_tank']['init_vals_hwt1']
-    init_vals_hwt2 = params['init_vals_tank']['init_vals_hwt2']
-    params_chp_uni = {
-        'eid_prefix' : 'CHP',
-        'heat_out' : [0, 92000],
-        'nom_P_th' : 92000,
-        # 'elec_share' : 0.5,
-        'P_el' : 46000,
-        'startup_coeff' : [-2.63, 3.9, 0.57],
-        'startup_limit' : 11,
-        'set_flow' : 4,
-        'step_size' : STEP_SIZE
-    }
-    params_boiler_uni = {'eid_prefix' : 'Boiler',
-        'heat_out' : [0, 74000, 148000, 222000, 296000, 370000], #Operating points of boiler, in W
-                            'set_temp' : 75}
+    init_vals_hwt2 = params['init_vals_tank']['init_vals_hwt2']    
+    params_chp = params['params_chp']
+    params_boiler = params['params_boiler']
+    params_chp['step_size'] = STEP_SIZE   
 
 
-    # Create World
-    world = mosaik.World(sim_config)
-    START = '2022-01-01 00:00:00'
-    END =  365*24*60*60 # one year in seconds.    
-    # -----------------------------------------pv---------------------------------------
-
+    # -----------------------------------------pv-------------------------------------------------------------------------------------
+    #Standalone pvmodel-------------------------------------------------
     pvlib_model.sim()
     pv_results = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'PVlib_output.csv')) 
     pv_csv = world.start('CSV', sim_start = START, datafile = pv_results)
 
     pv_mod = pv_csv.Data.create(1)
-
-
-    
-    #----------------------------------------pv_end---------------------------------------
-    
-
-
+        
+    #---------pvsimulator.py-----------------------------------------------------
     
     # parameters for pv model
     # LAT = 32.0
@@ -302,41 +162,38 @@ def run_DES(params):
     # EFF = 0.5
     # EL = 32.0
     # AZ = 0.0
-    # DNI_DATA = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data','inputs', 'solar_data_HSO.csv')) # pv
-    # METEO_DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data','inputs', 'Braunschweig_meteodata_2022_15min.csv') # pvlib
 
+    # DNI_DATA = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data','inputs', 'solar_data_HSO.csv')) # pv
+    # pv_sim = world.start("PVSim",start_date=START,step_size=STEP_SIZE) # pv
+    # DNI_sim = world.start("CSV", sim_start=START, datafile=DNI_DATA) # pv
+    
+    # pv_model = pv_sim.PV.create(1, latitude=LAT, area=AREA,efficiency=EFF, el_tilt=EL, az_tilt=AZ) # pv
+    # DNI_model = DNI_sim.DNI.create(1) # pv
+    
+    
+    #-------------PVlib(mosaik adapter )---------------------------------------------
     # pv_count = 1
     # pv_config = {str(i) : generate_configurations(Scenarios.BUILDING) for i in range(pv_count)}
 
-    # configure the simulator
-    heatpumpsim = world.start('HeatPumpSim', step_size=STEP_SIZE)
-    hwtsim0 = world.start('HotWaterTankSim', step_size=STEP_SIZE, config=params_hwt)
-    hwtsim1 = world.start('HotWaterTankSim', step_size=STEP_SIZE, config=params_hwt)
-    hwtsim2 = world.start('HotWaterTankSim', step_size=STEP_SIZE, config=params_hwt)
-    ctrlsim = world.start('ControllerSim', step_size=STEP_SIZE)
-    # chpsim = world.start('CHPSim', step_size=STEP_SIZE)
-    # chpsim = world.start('EnergyTransformer', step_size = STEP_SIZE, params = params_chp_uni)
-    chpsim = world.start('Chpsim_v2', step_size = STEP_SIZE, params = params_chp_uni)
-    # boilersim = world.start('EnergyTransformer', step_size = STEP_SIZE, params = params_boiler_uni)
-    boilersim = world.start('Boilersim_v2', step_size = STEP_SIZE, params = params_boiler_uni)
-
-    # boilersim = world.start('Boilersim', step_size = STEP_SIZE)
-
-
+    # METEO_DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data','inputs', 'Braunschweig_meteodata_2022_15min.csv') # pvlib
     # pv_sim_pvlib = world.start("PVsim_pvlib", start_date=START, step_size=STEP_SIZE, pv_data=pv_config,) # pvlib (takes 4:01 minutes for 1 year)
-    # pv_sim = world.start("PVSim",start_date=START,step_size=STEP_SIZE) # pv
-    # DNI_sim = world.start("CSV", sim_start=START, datafile=DNI_DATA) # pv
     # meteo_sim = world.start("CSV", sim_start=START, datafile=METEO_DATA) # pvlib (takes 13:16 minutes for 1 year)
+    
+    # pv_model_pvlib = pv_sim_pvlib.PVSim.create(pv_count) # PVlib
+    # meteo_model = meteo_sim.Braunschweig.create(1) # pvlib
 
-    # Input data csv
+    #------------------------------------------PV END-----------------------------------------------------------------------------------------------------------------  
+
+    # ----------------------Input data csv------------------------
     HEAT_LOAD_DATA = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data', 'inputs', 'Input_kfw55_2_el.csv'))
     # configure the simulator
     csv = world.start('CSV', sim_start=START, datafile=HEAT_LOAD_DATA)
     # Instantiate model
+    heat_load = csv.HEATLOAD.create(1)
 
-    # Output data storage
+    # ------------------Output data storage-----------------------
     prefix, hash_prefix = generatePrefix(params, ref_param_filename)
-    hash_prefix = ''
+    # hash_prefix = ''
     
     # configure the simulator
     csv_sim_writer = world.start('CSV_writer', start_date= START, date_format='%Y-%m-%d %H:%M:%S',
@@ -344,32 +201,30 @@ def run_DES(params):
 
     csv_debug_writer = world.start('CSV_writer', start_date='2022-01-01 00:00:00', date_format='%Y-%m-%d %H:%M:%S',
                                 output_file='utils/debug.csv')
-
     # Instantiate model
+    csv_writer = csv_sim_writer.CSVWriter(buff_size=15 * 60 * 60)
+    csv_debug = csv_debug_writer.CSVWriter(buff_size=15 * 60 * 60)
+    #-------------------------------------------------------------
+
+    # configure other simulators
+    heatpumpsim = world.start('HeatPumpSim', step_size=STEP_SIZE)
+    hwtsim0 = world.start('HotWaterTankSim', step_size=STEP_SIZE, config=params_hwt)
+    hwtsim1 = world.start('HotWaterTankSim', step_size=STEP_SIZE, config=params_hwt)
+    hwtsim2 = world.start('HotWaterTankSim', step_size=STEP_SIZE, config=params_hwt)
+    ctrlsim = world.start('ControllerSim', step_size=STEP_SIZE)
+    chpsim = world.start('Chpsim_v2', step_size = STEP_SIZE, params = params_chp)
+    boilersim = world.start('Boilersim_v2', step_size = STEP_SIZE, params = params_boiler)
+    
+    # Instantiate other models
     heatpump = heatpumpsim.HeatPump.create(1, params=params_hp)
-    chp = chpsim.Transformer.create(1, params=params_chp_uni)
-    # chp = chpsim.CHP.create(1, params=params_chp)
+    chp = chpsim.Transformer.create(1, params=params_chp)
     hwts0 = hwtsim0.HotWaterTank.create(1, params=params_hwt, init_vals=init_vals_hwt0)
     hwts1 = hwtsim1.HotWaterTank.create(1, params=params_hwt, init_vals=init_vals_hwt1)
     hwts2 = hwtsim2.HotWaterTank.create(1, params=params_hwt, init_vals=init_vals_hwt2)
     ctrls = ctrlsim.Controller.create(1, params=params_ctrl)
-    heat_load = csv.HEATLOAD.create(1)
+    boiler = boilersim.Transformer.create(1, params = params_boiler)
 
-    # boiler = boilersim.GasBoiler.create(1, params = params_boiler)
-    boiler = boilersim.Transformer.create(1, params = params_boiler_uni)
-
-
-    #? delete?
-    # pv_model_pvlib = pv_sim_pvlib.PVSim.create(pv_count) # PVlib
-    # pv_model = pv_sim.PV.create(1, latitude=LAT, area=AREA,efficiency=EFF, el_tilt=EL, az_tilt=AZ) # pv
-    # DNI_model = DNI_sim.DNI.create(1) # pv
-    # meteo_model = meteo_sim.Braunschweig.create(1) # pvlib
-
-    # Instantiate model
-    csv_writer = csv_sim_writer.CSVWriter(buff_size=15 * 60 * 60)
-    csv_debug = csv_debug_writer.CSVWriter(buff_size=15 * 60 * 60)
-
-    # Connect entities
+    # -------------------------------------------------------------Connect entities----------------------------------------------------------------------------
 
     world.connect(heat_load[0], ctrls[0], 'T_amb', ('Heat Demand [kW]', 'heat_demand'), ('Domestic hot water (kW)' ,  'dhw_demand'), ('Space heating (kW)', 'sh_demand')
                   , ('Timestamp', 'timestamp'), ('offset_Electricy demand[kW]', 'pred_el_demand'))
@@ -421,22 +276,8 @@ def run_DES(params):
                 ('heat_out.F', 'tes2_heat_out_F'), ('sensor_00.T', 'bottom_layer_Tank2'), ('sensor_02.T', 'top_layer_Tank2'), ('heat_out2.T','tes2_heat_out2_T'),
                 ('heat_out2.F', 'tes2_heat_out2_F'), ('sensor_01.T', 'middle_layer_Tank2'))
  
- 
+
     """__________________________________________Boiler_______________________________________________________________________"""
-
-    # world.connect(hwts2[0], boiler[0], ('sensor_00.T', 'temp_in'))
-    # world.connect(boiler[0], hwts2[0], ('temp_out', 'boiler_in.T'), ('mdot','boiler_in.F'), ('mdot_neg', 'boiler_out.F'),
-    #                 time_shifted=True, initial_data={'temp_out': 20, 'mdot':0, 'mdot_neg':0})
-    
-    # world.connect(boiler[0], ctrls[0], ('P_th', 'boiler_supply'), ('boiler_uptime','boiler_uptime'),
-    #             ('mdot', 'boiler_mdot'))
-    
-    # world.connect(ctrls[0], boiler[0], ('boiler_demand', 'Q_Demand'), 'boiler_status',
-    #             time_shifted=True,
-    #             initial_data={'boiler_demand': 0})
-    
-
-    """__________________________________________Boiler_UNI_______________________________________________________________________"""
 
     world.connect(hwts2[0], boiler[0], ('sensor_00.T', 'temp_in'))
     world.connect(boiler[0], hwts2[0], ('temp_out', 'boiler_in.T'), ('mdot','boiler_in.F'), ('mdot_neg', 'boiler_out.F'),
@@ -450,7 +291,7 @@ def run_DES(params):
                 initial_data={'boiler_demand': 0})
 
 
-    """__________________________________________CHP_UNI__________________________________"""  
+    """__________________________________________CHP__________________________________"""  
     
     world.connect(hwts2[0], chp[0], ('sensor_00.T', 'temp_in'))
     world.connect(chp[0], hwts2[0], ('temp_out', 'chp_in.T'), ('mdot','chp_in.F'), ('mdot_neg', 'chp_out.F'),
@@ -463,19 +304,6 @@ def run_DES(params):
                 time_shifted=True,
                 initial_data={'chp_demand': 90000}
                 )
-    """__________________________________________ CHP ________________________________________________________________________"""
-
-    # world.connect(hwts2[0], chp[0], ('sensor_00.T', 'temp_in'))
-    # world.connect(chp[0], hwts2[0], ('temp_out', 'chp_in.T'), ('mdot','chp_in.F'), ('mdot_neg', 'chp_out.F'),
-    #                 time_shifted=True, initial_data={'temp_out': 20, 'mdot':0, 'mdot_neg':0})
-
-    # world.connect(chp[0], ctrls[0], ('P_th', 'chp_supply'), ('chp_uptime', 'chp_uptime'), ('P_el', 'chp_el'),
-    #             ('mdot', 'chp_mdot')) 
-
-    # world.connect(ctrls[0], chp[0], ('chp_demand', 'Q_Demand'), ('chp_status' , 'chp_status'),
-    #             time_shifted=True,
-    #             initial_data={'chp_demand': 90000}
-    #             )
 
     """__________________________________________ heat pump ___________________________________________________________________""" 
 
@@ -497,7 +325,7 @@ def run_DES(params):
                 )
 
 
-    """__________________________________________ PV ___________________________________________________________________""" 
+    """__________________________________________ PVsim ___________________________________________________________________""" 
     #? delete connections?
     # world.connect(      DNI_model[0],
     #                     pv_model[0],
@@ -570,7 +398,7 @@ def run_DES(params):
     world.connect(boiler[0], csv_writer, 'P_th', 'Q_demand', 'temp_out', 'mdot')   
 
 
-    """__________________________________________ world run ______________________________________________________________"""
+    """__________________________________________ world run _______________________________________________________________________________________________________________"""
 
     
     # To start heatpump as first simulator
