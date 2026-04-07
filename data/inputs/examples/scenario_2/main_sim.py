f5a546a0f439b02e46cdd06e605b87db1043502e
hsalih <hsalih@stud.hs-offenburg.de>
2026-04-07 12:40:37 +0000

    added main simulation configuration guide

---
+++ data/inputs/examples/scenario_2/main_sim.py
@@ -0,0 +1,274 @@
+import mosaik
+import mosaik.util
+import os
+import sys
+import nest_asyncio
+nest_asyncio.apply()
+import logging
+import json
+
+#setup the logger
+logger = logging.getLogger("mosaik_logger")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_PATH = os.path.join(current_dir, "..", 'data/outputs')
+sys.path.append(os.path.join(current_dir, ".."))
+
+from src.models import pvlib_model
+STEP_SIZE = 60*15 # step size 15 minutes
+HV = 10833.3 #Heating value of natural gas in Wh/m^3; standard cubic meter
+
+def export2json(params_dict):
+    filename = os.path.join(OUTPUT_PATH, 'used_params.json')
+    with open(filename, 'w') as f:
+        json.dump(params_dict, f, indent = 4)
+
+def run_DES(params, collect=True, plot_graph=False):
+    sim_config = {
+        'EnergyTransformer' : {
+            'python' : 'src.models.EnergyTransformer_frame:TransformerSimulator',
+        },
+        'Boilersim_v2' : {
+            'python' : 'src.models.boiler_model_v2:TransformerSimulator'
+        },
+        'CSV': {
+            'python': 'mosaik_csv:CSV',
+        },
+        'CSV_writer': {
+            'python': 'mosaik_csv_writer:CSVWriter'
+        },
+        'HeatPumpSim': {
+            'python': 'mosaik_components.heatpump.Heat_Pump_mosaik:HeatPumpSimulator',
+        },
+        'HotWaterTankSim': {
+            'python': 'mosaik_components.heatpump.hotwatertank.hotwatertank_mosaik:HotWaterTankSimulator',
+        
+        },
+        'ControllerSim': {
+            'python': 'src.models.controller_mosaik:ControllerSimulator',
+            
+        },
+        'PVSim': {
+                'python': 'mosaik_components.pv.pvsimulator:PVSimulator',
+        },
+        'Collector': {
+                'python': 'src.models.collector:Collector',
+        }  
+    }
+    
+    # Create World
+    
+    world = mosaik.World(sim_config, mosaik_config={'addr':('127.0.0.1', 0)})
+    START = '2022-01-01 00:00:00'
+    END =  365*24*60*60 # one year in seconds
+
+    # unpacking input params
+    params_hp = params['hp']
+    params_hwt = params['tank']
+    params['ctrl']['tank'] = params['tank']
+    init_vals_hwt0 = params['init_vals_tank']['init_vals_hwt0']
+    init_vals_hwt1 = params['init_vals_tank']['init_vals_hwt1']
+    params_boiler = params['boiler']
+    params_pv = params['pv']
+
+    # -----------------------------------------pv-------------------------------------------------------------------------------------
+    #Standalone pvmodel-------------------------------------------------
+    pv_results = pvlib_model.sim(params_pv)
+    pv_csv = world.start('CSV', sim_start = START, datafile = pv_results)
+    pv_mod = pv_csv.Data.create(1)
+        
+    # ----------------------Input data csv------------------------
+    HEAT_LOAD_DATA = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data', 'inputs', 'Input_kfw55_2_el.csv'))
+    # configure the simulator
+    csv = world.start('CSV', sim_start=START, datafile=HEAT_LOAD_DATA)
+    # Instantiate model
+    heat_load = csv.HEATLOAD.create(1)
+
+    # ------------------Output data storage-----------------------
+    # configure the simulator
+    csv_sim_writer = world.start('CSV_writer', start_date= START, date_format='%Y-%m-%d %H:%M:%S',
+                                output_file=os.path.join(OUTPUT_PATH, 'DES_data.csv'))
+    # Instantiate model
+    collector = world.start('Collector')
+    csv_writer = csv_sim_writer.CSVWriter(buff_size=15 * 60 * 60)
+    col = collector.Collector()
+
+    #-------------------------------------------------------------
+    # configure other simulators
+    heatpumpsim = world.start('HeatPumpSim', step_size=STEP_SIZE)
+    hwtsim0 = world.start('HotWaterTankSim', step_size=STEP_SIZE, config={**params_hwt, "Tanknumber" : 0})
+    hwtsim1 = world.start('HotWaterTankSim', step_size=STEP_SIZE,config={**params_hwt, "Tanknumber" : 1})
+    ctrlsim = world.start('ControllerSim', step_size=STEP_SIZE, params = params['ctrl'])
+    boilersim = world.start('Boilersim_v2', step_size = STEP_SIZE, params = params_boiler)
+    
+    # Instantiate other models
+    heatpump = heatpumpsim.HeatPump.create(1, params=params_hp)
+    hwts0 = hwtsim0.HotWaterTank.create(1, params=params_hwt, init_vals=init_vals_hwt0)
+    hwts1 = hwtsim1.HotWaterTank.create(1, params=params_hwt, init_vals=init_vals_hwt1)
+    ctrls = ctrlsim.Controller.create(1, params=params['ctrl'])
+    boiler = boilersim.Transformer.create(1, params = params_boiler)
+    # -------------------------------------------------------------Connect entities----------------------------------------------------------------------------
+
+    world.connect(heat_load[0], ctrls[0], 'T_amb', ('Heat Demand [kW]', 'heat_demand'), ('Domestic hot water (kW)' ,  'dhw_demand'), ('Space heating (kW)', 'sh_demand')
+                  , ('Timestamp', 'timestamp'), ('offset_Electricy demand[kW]', 'pred_el_demand'))
+    world.connect(pv_mod[0], ctrls[0], ('Power[w]', 'pv_gen'))
+
+    """__________________________________________ hwts ___________________________________________________________________""" 
+
+    world.connect(hwts0[0], ctrls[0], ('heat_out.T', 'tank_connections.tank0.heat_out_T'), 
+              ('hp_out.T', 'hp_out_T'),('sensor_00.T', 'tank_temps.tank0.sensor_0'), ('heat_out2.T','tank_connections.tank0.heat_out2_T'), 
+              ('sensor_01.T', 'tank_temps.tank0.sensor_1'),('heat_out2.F', 'tank_connections.tank0.heat_out2_F'), 
+              ('sensor_02.T', 'tank_temps.tank0.sensor_2'),time_shifted=True, 
+              initial_data={'heat_out.T':0, 'hp_out.T':0, 'sensor_00.T':0, 
+                            'heat_out2.T' : 0, 'heat_out2.F':0})
+
+    world.connect(ctrls[0], hwts0[0], 
+              ('tank_connections.tank0.heat_out_F', 'heat_out.F'),
+              ('tank_connections.tank0.heat_out_T', 'heat_out.T'),
+              ('tank_connections.tank0.heat_in_F', 'heat_in.F'),
+              ('tank_connections.tank0.heat_in_T', 'heat_in.T'),
+              ('tank_connections.tank0.heat_out2_F', 'heat_out2.F'),
+              ('hwt0_hr_1', 'hr_1.P_th_set'))
+
+    world.connect(hwts1[0], ctrls[0], 
+              ('heat_out.T', 'tank_connections.tank1.heat_out_T'),('T_mean', 'T_mean_hwt'), 
+              ('mass', 'hwt_mass'),('sensor_02.T', 'tank_temps.tank1.sensor_2'),
+              ('hp_out.T', 'tank_connections.tank1.hp_out_T'),('heat_out2.T','tank_connections.tank1.heat_out2_T'),
+              ('heat_out2.F', 'tank_connections.tank1.heat_out2_F'),('sensor_01.T', 'tank_temps.tank1.sensor_1'), ('sensor_00.T', 'tank_temps.tank1.sensor_0'),
+              time_shifted=True, 
+              initial_data={'heat_out.T':0, 'heat_out2.T':0,'T_mean':0, 'mass':0, 
+                            'sensor_02.T':0, 'hp_out.T':0})
+
+    world.connect(ctrls[0], hwts1[0],
+        ('tank_connections.tank1.hp_out_F', 'hp_out.F'),
+        ('tank_connections.tank1.heat_out_T', 'heat_out.T'),
+        ('tank_connections.tank1.heat_out_F', 'heat_out.F'),
+        ('tank_connections.tank1.heat_out2_F', 'heat_out2.F'),
+        ('tank_connections.tank1.hp_out_T', 'hp_out.T'),
+        ('hwt1_hr_1', 'hr_1.P_th_set'),
+        ('tank_connections.tank1.heat_in_F', 'heat_in.F'),
+        ('tank_connections.tank1.heat_in_T', 'heat_in.T'),)
+
+
+
+    """__________________________________________Boiler_______________________________________________________________________"""
+
+    world.connect(hwts1[0], boiler[0], ('sensor_00.T', 'temp_in'), time_shifted=True, initial_data={'sensor_00.T':0})
+    world.connect(boiler[0], hwts1[0], ('temp_out', 'boiler_in.T'), ('mdot','boiler_in.F'), ('mdot_neg', 'boiler_out.F'),
+                    time_shifted=True, initial_data={'temp_out': 20, 'mdot':0, 'mdot_neg':0})
+    
+    world.connect(boiler[0], ctrls[0], ('P_th', 'generators.boiler_supply'), ('uptime','boiler_uptime'),
+                )
+    
+    world.connect(ctrls[0], boiler[0], ('generators.boiler_demand', 'Q_demand'), ('generators.boiler_status', 'status'),
+                time_shifted=True,
+                initial_data={'generators.boiler_demand': 0})
+
+
+    """__________________________________________ heat pump ___________________________________________________________________""" 
+
+    world.connect(heatpump[0], ctrls[0], ('Q_Supplied', 'generators.hp_supply'), ('on_fraction', 'hp_on_fraction'),
+                ('cond_m', 'hp_cond_m'), ('P_Required','HP_P_Required'))
+
+    world.connect(ctrls[0], heatpump[0], ('generators.hp_demand', 'Q_Demand'),
+                'T_amb', 'heat_source_T', time_shifted=True,
+                initial_data={'generators.hp_demand': 0, 'T_amb': 5, 'heat_source_T': 5})
+
+    world.connect(hwts0[0], heatpump[0], ('hp_out.T', 'cond_in_T'),
+                time_shifted=True, initial_data={'hp_out.T':0}
+                )
+
+    world.connect(heatpump[0], hwts0[0], ('cons_T', 'hp_in.T'), ('cond_m', 'hp_in.F') ,('cond_m_neg', 'hp_out.F'),
+                )
+
+    world.connect(heatpump[0], ctrls[0], ('cond_m_neg', 'tank_connections.tank0.hp_out_F'), ('cond_m', 'tank_connections.tank0.hp_in_F'),)
+
+
+    """__________________________________________ CSV ___________________________________________________________________""" 
+    # connect everything to the csv writer
+    world.connect(heat_load[0], csv_writer, 'T_amb', 'Heat Demand [kW]', 'Domestic hot water (kW)', 'Space heating (kW)')
+    world.connect(heatpump[0], csv_writer, 'Q_Demand', 'Q_Supplied', 'T_amb', 'heat_source_T', 'cons_T',
+                'P_Required',
+                'COP', 'cond_m', 'cond_in_T', 'on_fraction','Q_evap')
+
+    world.connect(ctrls[0], csv_writer, 'heat_demand', 'heat_supply', 'generators.hp_demand', 'generators.hp_supply',
+                 'sh_supply', 'dhw_supply', 
+                 'pv_gen',
+                 'IdealHrodsum', 'generators.boiler_demand', 'chp_uptime', 'req_shTsup', 'dch_power')
+
+    world.connect(hwts0[0], csv_writer, 'sensor_00.T', 'sensor_01.T', 'sensor_02.T', 
+                'heat_out.T', 'heat_out.F', 'hp_in.T', 'hp_in.F', 'hp_out.T',
+                'hp_out.F', 'heat_in.T', 'heat_in.F','heat_out2.F', 'heat_out2.T',
+                'T_mean')
+
+    world.connect(hwts1[0], csv_writer, 'sensor_00.T', 'sensor_01.T', 'sensor_02.T', 
+                'heat_out.T', 'heat_out.F', 'hp_in.T', 'hp_in.F', 'hp_out.T',
+                'hp_out.F', 'heat_in.T', 'heat_in.F', 'heat_out2.F', 'heat_out2.T',
+                'T_mean')
+ 
+    world.connect(boiler[0], csv_writer, 'P_th', 'Q_demand', 'temp_out', 'mdot')   
+
+    # auto-connect *all* source attributes to collector
+    def connect_all_attrs(world, src_sim, src_entities, collector_ent):
+        for e in src_entities:
+            model = e.type
+            attrs = src_sim.meta['models'][model]['attrs']  # list of attribute names
+            if attrs:
+                world.connect(e, collector_ent, *attrs)     # connect each attr by name
+                
+    connect_all_attrs(world, boilersim, boiler, col)
+    connect_all_attrs(world, ctrlsim, ctrls, col)
+    connect_all_attrs(world, heatpumpsim, heatpump, col)
+    # connect_all_attrs(world, hwtsim0, hwts0, col)
+    # connect_all_attrs(world, hwtsim1, hwts1, col)
+    # connect_all_attrs(world, hwtsim2, hwts2, col)    
+    connect_all_attrs(world, csv, heat_load, col)
+    
+    """__________________________________________ world run _______________________________________________________________________________________________________________"""
+
+    
+    # To start heatpump as first simulator
+    world.set_initial_event(heatpump[0].sid)
+    
+    data = None
+    if collect == True:
+        data = collector.dump() 
+
+    # Run simulation
+    world.run(until=END)
+
+    # plot the data flow
+    if plot_graph == True:
+        mosaik.util.plot_dataflow_graph(world, folder=os.path.join(current_dir, 'utils/util_figures'), show_plot=False)
+
+    return data
+    
+    #logger message
+    # logger.info(f"Scenario successfully simulated : {hash_prefix}.") #It is possible to have different logger levels depending on how important the information of the logger is.
+    # Levels are (debug, info, warning, error)
+    # print(f'\n output : {hash_prefix, prefix}')
+    # export2json(params) #Exporting current parameters to a json, to be available to compare in next iteration.
+    
+    #warning log
+    # logger.warning("Result of the simulation is:" +str(result))
+
+    
+
+# def pvsim():
+#     pvlib_model.sim()
+
+if __name__ == "__main__":  
+   
+    # unpacking parameters from teh input json
+    filename = 'input_params.json'
+    path = os.path.join('..', 'data', 'inputs', filename)
+    with open(path, 'r') as f:
+        params = json.load(f)
+    
+    run_DES(params) #this will be executed only when this file is run directly.
+    
+    # pvsim()
+# import cProfile
+# import pstats
+# cProfile.run('run_DES()', 'profile_output') 
+# p = pstats.Stats('profile_output')
---
+++ docs/main_sim.md
@@ -0,0 +1,225 @@
+# main_sim — Configuration Guide
+
+## Overview
+
+The main simulation file acts as the integration layer of the DES setup.
+Its role is to assemble configured subsystem models into one executable scenario, run the simulation timeline, and export results.
+
+Model-level behavior is defined in each model block.
+System-level behavior depends on how those blocks are combined and connected in the simulation run.
+
+## Input setup
+
+The simulation is configured through one input dictionary.
+That dictionary is structured as named configuration blocks.
+
+Input file used by the run:
+- [data/inputs/input_params.json](../data/inputs/input_params.json)
+
+Reference examples (planned):
+- [data/inputs/examples/](../data/inputs/examples/)
+
+The active input file is intended for scenario-specific edits.
+The examples folder is intended as a reference library for alternative scenario setups.
+
+### Core blocks
+
+These blocks define scenario structure and startup state:
+
+- `ctrl` — controller logic, supply routing, and switching behavior
+- `tank` — tank geometry, ports, sensor layout, and thermal properties
+- `init_vals_tank` — initial tank temperatures and startup state
+
+### Component blocks (scenario-dependent)
+
+These blocks are included only when the component is active in the scenario:
+
+- `hp` — include when heat pump is part of the run
+- `chp` — include when CHP is part of the run
+- `boiler` — include when boiler is part of the run
+- `pv` — include when PV generation is part of the run
+- `battery` — include when electrical storage is part of the run
+
+### Example of one configuration block
+```json
+"boiler": {
+  "nom_P_th": 200000,
+  "set_temp": 75,
+  "efficiency": 0.8,
+  "heating_value": 10833.3
+}
+```
+## Setup flow
+
+A stable setup usually follows this order:
+
+1. Define scenario composition
+   Decide which components are active and which blocks are needed.
+
+2. Configure the system core
+   Set `tank`, `init_vals_tank`, and `ctrl` first, so routing and control references have a stable base.
+
+3. Add active component blocks
+   Add `hp`, `chp`, `boiler`, `pv`, and `battery` only where used.
+
+4. Align control references with active components
+   Ensure `ctrl.gens` and `ctrl.logic` reflect only the components intended for the simulation.
+
+5. Run consistency checks before execution
+   Verify naming and references across blocks, not only values inside single blocks.
+
+## Configuration consistency checks
+
+Some issues are caught by individual model validation, but many setup problems are integration-level and appear only when blocks are combined.
+
+### Generator alignment
+
+Expected pattern:
+- `ctrl.gens` lists active generators
+- `ctrl.logic` contains matching generator entries (see [Controller — Generator control logic](controller.md#generator-control-logic) for detailed logic structure and options)
+- corresponding component blocks exist in input
+
+Example:
+```json
+"ctrl": {
+  "gens": ["hp", "boiler"],
+  "logic": {
+    "hp": { ... },
+    "boiler": { ... }
+  }
+}
+
+"hp": { ... }
+"boiler": { ... }
+```
+### Tank and port alignment
+
+Expected pattern:
+- tank IDs used in control logic exist in the scenario
+- ports referenced in routing/balancing exist in `tank.connections`
+
+Example:
+```json
+"TankbalanceSetup": [
+  "tank0.heat_out:tank1.hp_out"
+]
+
+"tank": {
+  "connections": {
+    "heat_out": { ... },
+    "hp_out": { ... }
+  }
+}
+```
+### Initialization alignment
+
+Expected pattern:
+- all runtime tank instances have matching initial-value entries in `init_vals_tank`
+
+Example:
+```json
+"init_vals_tank": {
+  "init_vals_hwt0": { ... },
+  "init_vals_hwt1": { ... },
+  "init_vals_hwt2": { ... }
+}
+```
+## Implementation notes
+
+The orchestration code in main_sim consists of coupled sections that must stay synchronized.
+
+### Understanding the orchestration structure
+
+For background on main simulation setup concepts in mosaik (world, simulators, entities, and connections), see:
+
+- [Scenario API and setup documentation](https://mosaik.readthedocs.io/en/latest/scenario-definition.html)
+- [mosaik GitLab repository docs](https://gitlab.com/mosaik/mosaik/-/tree/develop/docs?ref_type=heads)
+
+The following section describes how those concepts are applied in this project-specific main_sim setup.
+
+The main_sim function performs these distinct operations in order:
+
+1. Simulator registry setup (sim_config dict)
+   Declares which simulator classes will be used.
+
+2. Model instantiation (world.start and .create calls)
+   Creates entity instances from the registered simulators.
+
+3. Signal wiring (world.connect calls)
+   Establishes data flow between entities.
+
+4. Data export setup (csv_writer and collector connections)
+   Routes model outputs to result files and memory.
+
+5. Execution (world.run)
+   Runs the assembled scenario.
+
+### What happens when sections are inconsistent
+
+- If a component is registered but not instantiated, the registry is unused.
+- If a component is instantiated but not connected, its signals are not available.
+- If a component is connected but not exported, its output is lost.
+- If a component is removed from input but still wired in main_sim, entity-not-found errors occur at runtime.
+
+### General pattern for modifications
+
+When configuration changes are planned:
+
+1. Update input_params.json
+   Add or remove the component block and controller references.
+
+2. Update simulator registry
+   Add or remove the simulator entry in sim_config.
+
+3. Update instantiation
+   Add or remove world.start() and .create() calls.
+
+4. Update signal wiring
+   Add or remove world.connect() calls for that component.
+
+5. Update data export
+   Add or remove csv_writer and collector connections.
+
+### Finding sections in main_sim.py
+
+Each section is logically grouped but scattered through the code:
+- Simulator registry: near the start of run_DES function
+- Instantiation: in the entity creation block
+- Signal wiring: in the world.connect section
+- Data export: in the CSV writer and collector section
+- Execution: world.run() call near the end
+
+When modifying, search for the component name throughout the file to ensure all occurrences are updated.
+
+## Example scenarios
+
+The following scenarios provide runnable references.  
+Each scenario contains a paired `main_sim.py` and `input_params.json`.
+
+### Scenario 1 
+
+
+- Components: hp
+- Number of Tanks: 1
+- main_sim file: [main_sim.py](../data/inputs/examples/scenario_1/main_sim.py)
+- Inputs file: [input_params.json](../data/inputs/examples/scenario_1/input_params.json)
+
+![Scenario 1 diagram](../data/inputs/examples/scenario_1/Scenario1_diagram.png)
+### Scenario 2 
+
+
+- Components: hp, pv, boiler
+- Number of Tanks: 2
+- main_sim file: [main_sim.py](../data/inputs/examples/scenario_2/main_sim.py)
+- Inputs file: [input_params.json](../data/inputs/examples/scenario_2/input_params.json)
+
+![Scenario 2 diagram](../data/inputs/examples/scenario_2/Scenario2_diagram.png)
+
+### Scenario 3
+
+- Components: hp, boiler, pv, battery, chp
+- Number of Tanks: 3
+- main_sim file: [main_sim.py](../data/inputs/examples/scenario_3/main_sim.py)
+- Inputs file: [input_params.json](../data/inputs/examples/scenario_3/input_params.json)
+
+![Scenario 3 diagram](../data/inputs/examples/scenario_3/Scenario3_diagram.png)
